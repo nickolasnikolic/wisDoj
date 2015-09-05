50793adc63130a83319c29da385b90822ed7e0c9
Nickolas Nikolic <nickolasnikolic@gmail.com>
2015-09-05 17:44:25 -0500

    first push

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  //set the stage
+  var s = Snap('#doj');
+  //layout elements
+    //img
+  var bg = s.image('images/overdose.jpg', 0,0,1920,1285);
+  bg.animate({y: -1285}, 5000);
+    //pain text
+    //killers
+    //dots
+});
